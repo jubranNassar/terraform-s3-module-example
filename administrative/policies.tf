fd0111e6f4482509529494ddce6e4eecdabd7f3d
jubran nassar <jubrann@spacelift.io>
2024-02-16 18:39:01 -0500

    fixes space argument

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -1,6 +1,6 @@
 resource "spacelift_policy" "trigger-consumers" {
     name = "trigger-consumers"
     type = "TRIGGER"
-    space = data.spacelift_current_space.this.id
+    space_id = data.spacelift_current_space.this.id
     body = "../policies/trigger/trigger-consumers.rego"
 }

9ca78f7159f78dfe0243cf20bf40fe1ad4f4a730
jubran nassar <jubrann@spacelift.io>
2024-02-16 17:02:53 -0500

    fixes repo name

--- administrative/modules.tf
+++ administrative/modules.tf
@@ -1,7 +1,7 @@
 resource "spacelift_module" "s3-module" {
   name         = "terraform-aws-s3"
   branch       = "main"
-  repository   = "terraform-s3-module"
+  repository   = "terraform-s3-module-example"
   project_root = "s3-module"
   space_id     = data.spacelift_current_space.this.id
 

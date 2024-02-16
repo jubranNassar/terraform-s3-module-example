ec8aa16a33c15f05e7e1d4abb5723dbc50e62976
jubran nassar <jubrann@spacelift.io>
2024-02-16 18:37:51 -0500

    adds missing argument

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -1,4 +1,5 @@
 resource "spacelift_policy" "trigger-consumers" {
+    name = "trigger-consumers"
     type = "TRIGGER"
     space = data.spacelift_current_space.this.id
     body = "../policies/trigger/trigger-consumers.rego"

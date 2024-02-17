2992862db9549c48a0ed7c00dc4ab47559d8accb
jubran nassar <jubrann@spacelift.io>
2024-02-16 19:01:35 -0500

    updates version

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,6 +1,6 @@
 module "terraform-aws-s3" {
   source  = "spacelift.io/my-first-env/terraform-aws-s3/default"
-  version = "0.1.2"
+  version = ">= 0.1.3"
 
   bucket_name = "this-is-jubrans-test-bucket-23223"
 }

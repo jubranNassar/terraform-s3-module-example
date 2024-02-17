ddd9af1529ef5ed94b86dfd0013377dd236701af
jubran nassar <jubrann@spacelift.io>
2024-02-16 19:10:20 -0500

    updates module

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -4,3 +4,8 @@ resource "spacelift_policy" "trigger-consumers" {
   space_id = data.spacelift_current_space.this.id
   body     = file("../policies/trigger/trigger-consumers.rego")
 }
+
+resource "spacelift_polict_attachment" "this" {
+    module_id = spacelift_module.s3-module.id
+    policy_id = spacelift_policy.trigger-consumers.id
+}
--- s3-module/main.tf
+++ s3-module/main.tf
@@ -2,7 +2,8 @@ resource "aws_s3_bucket" "example-bucket" {
   bucket = var.bucket_name
 
   tags = {
-    name: "test"
+    name: "test",
+    more: "tags"
   }
 }
 
